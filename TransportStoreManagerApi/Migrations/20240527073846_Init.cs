f56d1b03bb272a695eb727b5ea0daaf6f3beee9d
Vladyslav Dubok <vladislav.dubok@itomy.ch>
2024-05-27 10:45:58 +0300

    add migrations

---
+++ TransportStoreManagerApi/Migrations/20240527073846_Init.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportStoreManagerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlobFile",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlobFile", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brand", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currency",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currency", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fullname = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductType",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Promotion",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Percent = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promotion", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Count = table.Column<long>(type: "bigint", nullable: false),
+                    BrandId = table.Column<long>(type: "bigint", nullable: false),
+                    ProductTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    CustomerId = table.Column<long>(type: "bigint", nullable: false),
+                    CurrencyId = table.Column<long>(type: "bigint", nullable: false),
+                    BrandId1 = table.Column<long>(type: "bigint", nullable: true),
+                    CurrencyId1 = table.Column<long>(type: "bigint", nullable: true),
+                    CustomerId1 = table.Column<long>(type: "bigint", nullable: true),
+                    ProductTypeId1 = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Brand_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brand",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Product_Brand_BrandId1",
+                        column: x => x.BrandId1,
+                        principalTable: "Brand",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Product_Currency_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Product_Currency_CurrencyId1",
+                        column: x => x.CurrencyId1,
+                        principalTable: "Currency",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Product_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Product_Customer_CustomerId1",
+                        column: x => x.CustomerId1,
+                        principalTable: "Customer",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Product_ProductType_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Product_ProductType_ProductTypeId1",
+                        column: x => x.ProductTypeId1,
+                        principalTable: "ProductType",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductPhoto",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    FileId = table.Column<long>(type: "bigint", nullable: false),
+                    BlobFileId = table.Column<long>(type: "bigint", nullable: true),
+                    ProductId1 = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductPhoto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductPhoto_BlobFile_BlobFileId",
+                        column: x => x.BlobFileId,
+                        principalTable: "BlobFile",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProductPhoto_BlobFile_FileId",
+                        column: x => x.FileId,
+                        principalTable: "BlobFile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductPhoto_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductPhoto_Product_ProductId1",
+                        column: x => x.ProductId1,
+                        principalTable: "Product",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductPromotion",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    PromotionId = table.Column<long>(type: "bigint", nullable: false),
+                    ProductId1 = table.Column<long>(type: "bigint", nullable: true),
+                    PromotionId1 = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductPromotion", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductPromotion_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductPromotion_Product_ProductId1",
+                        column: x => x.ProductId1,
+                        principalTable: "Product",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProductPromotion_Promotion_PromotionId",
+                        column: x => x.PromotionId,
+                        principalTable: "Promotion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductPromotion_Promotion_PromotionId1",
+                        column: x => x.PromotionId1,
+                        principalTable: "Promotion",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductPromotionHistory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Percent = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    ProductPromotionId = table.Column<long>(type: "bigint", nullable: false),
+                    ProductPromotionId1 = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductPromotionHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductPromotionHistory_ProductPromotion_ProductPromotionId",
+                        column: x => x.ProductPromotionId,
+                        principalTable: "ProductPromotion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductPromotionHistory_ProductPromotion_ProductPromotionId1",
+                        column: x => x.ProductPromotionId1,
+                        principalTable: "ProductPromotion",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_BrandId",
+                table: "Product",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_BrandId1",
+                table: "Product",
+                column: "BrandId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CurrencyId",
+                table: "Product",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CurrencyId1",
+                table: "Product",
+                column: "CurrencyId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CustomerId",
+                table: "Product",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CustomerId1",
+                table: "Product",
+                column: "CustomerId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductTypeId",
+                table: "Product",
+                column: "ProductTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductTypeId1",
+                table: "Product",
+                column: "ProductTypeId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPhoto_BlobFileId",
+                table: "ProductPhoto",
+                column: "BlobFileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPhoto_FileId",
+                table: "ProductPhoto",
+                column: "FileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPhoto_ProductId",
+                table: "ProductPhoto",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPhoto_ProductId1",
+                table: "ProductPhoto",
+                column: "ProductId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPromotion_ProductId",
+                table: "ProductPromotion",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPromotion_ProductId1",
+                table: "ProductPromotion",
+                column: "ProductId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPromotion_PromotionId",
+                table: "ProductPromotion",
+                column: "PromotionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPromotion_PromotionId1",
+                table: "ProductPromotion",
+                column: "PromotionId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPromotionHistory_ProductPromotionId",
+                table: "ProductPromotionHistory",
+                column: "ProductPromotionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPromotionHistory_ProductPromotionId1",
+                table: "ProductPromotionHistory",
+                column: "ProductPromotionId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxMessages");
+
+            migrationBuilder.DropTable(
+                name: "ProductPhoto");
+
+            migrationBuilder.DropTable(
+                name: "ProductPromotionHistory");
+
+            migrationBuilder.DropTable(
+                name: "BlobFile");
+
+            migrationBuilder.DropTable(
+                name: "ProductPromotion");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Promotion");
+
+            migrationBuilder.DropTable(
+                name: "Brand");
+
+            migrationBuilder.DropTable(
+                name: "Currency");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "ProductType");
+        }
+    }
+}
